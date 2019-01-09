12b0915d857953873da0baed562d297010df92b8
José Robles Bastidas <JoseRoblesBastidas@hotmail.com>
2019-01-09 20:44:42 +0100

    First Launch

---
+++ Lambda/Lambda.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author José Robles Bastidas
+ */
+package Lambda;
+public class Lambda {
+    public int[] functionSteps;
+    public Lambda(String function){
+        this.functionSteps = this.getSteps(function);
+    }
+    public double get(double number){
+        return 0.0;
+    }
+    public int[] getSteps(String function){
+        boolean isParenthesis = true;
+        while(isParenthesis){
+
+        }
+        return new int[0];
+    }
+}
